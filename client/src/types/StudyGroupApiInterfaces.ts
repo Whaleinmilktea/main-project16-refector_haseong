30b11d4ef199b6674c58bf86e5363f420e9ded2a
KangHaSeong <shaawwert6044@gmail.com>
2023-07-12 15:46:29 +0900

    스터디 그룹 api 요청 함수 타입 분리

---
+++ client/src/types/StudyGroupApiInterfaces.ts
@@ -0,0 +1,96 @@
+export interface StudyGroup {
+  id: number;
+  title: string;
+  tagValues: string[];
+}
+
+export interface StudyGroupListDto {
+  leaders: StudyGroup[];
+  members: StudyGroup[];
+}
+
+export interface WaitingStudyGroupItemDto {
+  id: number;
+  title: string;
+}
+
+export interface WaitingStudyGroupListDto {
+  beStudys: WaitingStudyGroupItemDto[];
+}
+
+export interface StudyTags {
+  [key: string]: string[];
+}
+
+export interface StudyInfoDto {
+  id: number;
+  studyName: string;
+  studyPeriodStart: string;
+  studyPeriodEnd: string;
+  daysOfWeek: string[];
+  studyTimeStart: string;
+  studyTimeEnd: string;
+  memberCountMin: number;
+  memberCountMax: number;
+  memberCountCurrent: number;
+  platform: string;
+  introduction: string;
+  isRecruited: boolean;
+  tags: StudyTags;
+  leaderNickName: string;
+  leader: boolean;
+}
+
+export interface StudyGroupUpdateDto {
+  id?: number;
+  studyName: string;
+  studyPeriodStart: string;
+  studyPeriodEnd: string;
+  daysOfWeek: string[];
+  studyTimeStart: string;
+  studyTimeEnd: string;
+  memberCountMin: number;
+  memberCountMax: number;
+  platform: string;
+  introduction: string;
+  tags: StudyTags;
+}
+
+export interface StudyGroupUpdateDto {
+  id?: number;
+  studyName: string;
+  studyPeriodStart: string;
+  studyPeriodEnd: string;
+  daysOfWeek: string[];
+  studyTimeStart: string;
+  studyTimeEnd: string;
+  memberCountMin: number;
+  memberCountMax: number;
+  platform: string;
+  introduction: string;
+  tags: StudyTags;
+}
+
+export interface StudyGroupRecruitmentStatusUpdateDto {
+  state: boolean;
+}
+
+export interface StudyGroupMemberApprovalDto {
+  nickName: string;
+}
+
+export interface StudyGroupMemberApprovalDto {
+  nickName: string;
+}
+
+export interface StudyGroupMemberWaitingListDto {
+  nickName: [string];
+}
+
+export interface StudyGroupMemberListDto {
+  nickName: [string];
+}
+
+export interface StudyListOrderDto {
+  data: StudyGroup[];
+}
