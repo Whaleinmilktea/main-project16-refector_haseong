d9f8d28efe68c5c5bea142c07cc0db546c7fc945
KangHaSeong <shaawwert6044@gmail.com>
2023-08-03 00:10:57 +0900

    api 설계

--- client/src/apis/CalendarApi.ts
+++ client/src/apis/CalendarApi.ts
@@ -1,23 +1,24 @@
-import tokenRequestApi from "./TokenRequestApi";
-import { CalendarEvent, Schedule } from "../types/CalendarInterfaces";
 import { v4 as uuidv4 } from "uuid";
+import tokenRequestApi from "./TokenRequestApi";
+import { CalendarEvent, Schedule, ScheduleList } from "../types/CalendarInterfaces";
+import { DayOfWeekBinaryToNumber } from "../pages/utils/DaysOfWeekMap";
 
-export const getScheduleList = async () => {
+export const getScheduleList = async ()  => {
   const everySchedule: Schedule[] = [];
-  const response = await tokenRequestApi.get("/schedule/list");
+  const response = await tokenRequestApi.get<ScheduleList>("/schedule/list");
   everySchedule.push(...response.data.schedule); // 서버데이터 무결성 확보 ===> 새로운 배열에 넣기
 
-  const mappedSchedule: CalendarEvent[] = everySchedule.map((schedule) => {
-    const mappedSchedule: CalendarEvent = {
-      id: uuidv4(),
-      title: schedule.title,
-      startTime: schedule.startTime,
-      endTime: schedule.endTime,
-      startRecur: schedule.startDate,
-      endRecur: schedule.endDate,
-      description: schedule.description,
-      overlap: true,
-    };
-  });
+  const mappedSchedule: CalendarEvent[] = everySchedule.map((schedule) => ({
+    id: uuidv4(),
+    title: schedule.title,
+    daysOfWeek: DayOfWeekBinaryToNumber(schedule.dayOfWeek),
+    startTime: schedule.startTime,
+    endTime: schedule.endTime,
+    startRecur: schedule.startDate,
+    endRecur: schedule.endDate,
+    description: schedule.description,
+    overlap: true,
+  }));
+
   return mappedSchedule;
 };
--- client/src/components/calendar/Calendar.tsx
+++ client/src/components/calendar/Calendar.tsx
@@ -1,3 +1,18 @@
-const Calendar = () => {}
+import { useEffect } from "react";
+import { getScheduleList } from "../../apis/CalendarApi";
+import FullCalendar from "@fullcalendar/react";
+
+const Calendar = () => {
+
+  useEffect(() => {
+    getScheduleList();
+  }, []);
+
+  return (
+    <>
+      <FullCalendar />
+    </>
+  );
+};
 
 export default Calendar;
